c8bb43bdf7f0fbd57d45e6456db31dd77be7100c
onifade617 <onifade617@gmail.com>
2024-08-06 10:21:59 +0100

    second commit

--- main_page.py
+++ main_page.py
@@ -7,7 +7,7 @@ Created on Tue Aug  6 09:34:44 2024
 
 import streamlit as st
 
-import home, univariate, bi-variate, ml-model 
+import home, univariate, bi_variate, ml_model 
 
 # Create a menu
 st.sidebar.title("Departmental Collaboration Analysis")
@@ -21,16 +21,16 @@ home.main()
 if st.sidebar.button('Univariate Statistics'):
     st.session_state.page = 'univariate'
 if st.sidebar.button('Bi-Variate Statistics'):
-    st.session_state.page = 'bi-variate'
+    st.session_state.page = 'bi_variate'
 if st.sidebar.button('Machine Learning Models'):
-    st.session_state.page = 'ml-models'
+    st.session_state.page = 'ml_models'
     
     
 
 # Display the selected page
 if st.session_state.get('page') == 'univariate':
     univariate.main()
-elif st.session_state.get('page') == 'bi-variate':
+elif st.session_state.get('page') == 'bi_variate':
     bi-variate.main()
-elif st.session_state.get('page') == 'ml-models':
+elif st.session_state.get('page') == 'ml_models':
     ml-models.main()
