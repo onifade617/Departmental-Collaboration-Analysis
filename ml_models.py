e2022d380b6b53e0f838c124944fc613c4259eb0
onifade617 <onifade617@gmail.com>
2024-08-06 12:25:25 +0100

    ml_models commit 3

--- ml_models.py
+++ ml_models.py
@@ -10,6 +10,6 @@ import streamlit as st
 
 
 def main():    
-    st.write("Machine learning models")
+    st.title("Machine learning models")
     tab1, tab2 = st.tabs(["Supervised learning", "Unsupervised Learning"])
 
