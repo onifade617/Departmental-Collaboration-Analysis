8ebf6d474a5f928adf14099221ed1cb571d644e1
onifade617 <onifade617@gmail.com>
2024-08-06 11:05:46 +0100

    Univariate second commit

--- univariate.py
+++ univariate.py
@@ -7,8 +7,48 @@ Created on Tue Aug  6 09:53:18 2024
 
 import streamlit as st
 
-
+container1  = st.container() #container1
+container2  = st.container() #container2
+container3  = st.container() #container3
 
 def main():
     
     st.write("Univariate")
+    with container1:
+        col1, col2, col3 = st.columns(3)
+        
+        with col1:
+            st.markdown("## Variable 1")
+            
+        with col2:
+            st.markdown("## Variable 2")
+            
+        with col3:
+            st.markdown("## Variable 3")
+            
+    #Container 2
+    with container2:
+        col4, col5, col6 = st.columns(3)
+        
+        with col4:
+            st.markdown("## Variable 4")
+            
+        with col5:
+            st.markdown("## Variable 5")
+            
+        with col6:
+            st.markdown("## Variable 6")
+            
+    #Container 3
+    with container2:
+        col7, col8, col9 = st.columns(3)
+        
+        with col4:
+            st.markdown("## Variable 7")
+            
+        with col5:
+            st.markdown("## Variable 8")
+            
+        with col6:
+            st.markdown("## Variable 9")
+    
