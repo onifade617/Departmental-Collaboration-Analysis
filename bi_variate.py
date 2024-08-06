a15c9b5e7e29886c7e5f8f72dec7aa80fecc31a8
onifade617 <onifade617@gmail.com>
2024-08-06 11:33:08 +0100

    bivariate commit 3

--- bi_variate.py
+++ bi_variate.py
@@ -11,5 +11,5 @@ import streamlit as st
 def main():
     st.write("Bivariate")
     
-    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Tab 1", "Tab 2", "Tab 3", "Tab 4", "Tab 5"])
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Topic Trends", " Departmental Focus", "Topic Sentiment", "Collaboration Patterns", "Topic Device Correlation"])
 
