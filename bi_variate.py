5f9adafa7c0d7742022b73821a3bb1d89ebb6d4f
onifade617 <onifade617@gmail.com>
2024-08-08 10:13:09 +0100

    Update bi_variate.py

--- bi_variate.py
+++ bi_variate.py
@@ -9,7 +9,37 @@ import streamlit as st
 
 
 def main():
-    st.write("Bivariate")
+    st.title("Bivariate")
     
-    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Topic Trends", " Departmental Focus", "Topic Sentiment", "Collaboration Patterns", "Topic Device Correlation"])
+    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Communication Patterns During Work Hours",
+                                     "Workday Communication Trends",
+                                     "Executive Communication During Work Hours", 
+                                     "Departmental Communication During Work Hours", 
+                                     "Email Sentiment Analysis During Work Hours",
+                                     "Device Usage During Work Hours"])
 
+    with tab1:
+        st.title("Communication Patterns During Work Hours")
+        
+        
+        
+    with tab2:
+        st.title("Workday Communication Trends")
+    
+        
+    with tab3:
+        st.title("Executive Communication During Work Hours")
+        
+        
+    with tab4:
+        st.title("Departmental Communication During Work Hours")
+        
+        
+        
+    with tab5:
+        st.title("Email Sentiment Analysis During Work Hours")
+    
+        
+    with tab6:
+        st.title("Device Usage During Work Hours")
+        
